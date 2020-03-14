fb55a1cc2987891cd7fa60c7dde30c9cb0381a47
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-14 16:20:06 +0100

    Prepare reports file to be filled

--- reports.py
+++ reports.py
@@ -1,2 +1,50 @@
+def count_games(file_name):
+    """How many games are in the file?"""
+    return number
+    pass
 
-# Report functions
+
+def decide(file_name, year):
+    """Is there a game from a given year?"""
+    return True or False
+    pass
+
+
+def get_latest(file_name):
+    """Which was the latest game?"""
+    if more than one return first occurred in file
+    return str(latest_title)
+    pass
+
+
+def count_by_genre(file_name, genre):
+    """How many games do we have by genre?"""
+    return number
+    pass
+
+
+def get_line_number_by_title(file_name, title):
+    """What is the line number of the given game (by title)?"""
+    if no game found of a given title raise ValueError
+    return number
+    pass
+
+
+def sort_abc(file_name):
+    """What is the alphabetical ordered list of the titles?"""
+    implement your own sorting algorithm
+    return list of strings
+    pass
+
+
+def get_genres(file_name):
+    """What are the genres?"""
+    return distinct alphabetical list of genres
+    pass
+
+
+def when_was_top_sold_fps(file_name):
+    """What is the release date of the top sold "First-person shooter" game?"""
+    if no FPS game on list raise ValueError
+    return year as int
+    pass
